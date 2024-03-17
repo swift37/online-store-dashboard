333d2f895be9bc20fe18829bed7febd00583c848
Swift <swiftx37@gmail.com>
2024-03-17 21:53:26 +0100

    Added the ability to add an icon to the DefaultInputComponent.

--- src/app/shared/components/default-input/default-input.component.ts
+++ src/app/shared/components/default-input/default-input.component.ts
@@ -1,16 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-default-input',
   templateUrl: './default-input.component.html',
   styleUrl: './default-input.component.scss',
 })
-export class DefaultInputComponent {
+export class DefaultInputComponent implements OnChanges {
   @Input() type: string = 'text';
   @Input() id?: string;
   @Input() value?: any;
   @Input() placeholder?: string;
   @Input() label?: string;
   @Input() iconName?: string;
+  @Input() iconPosition: string = 'left';
   @Input() disabled: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('change');
+    if (this.iconPosition != 'left' && this.iconPosition != 'right') {
+      console.error("Icon position should be 'left' or 'right'");
+    }
+  }
 }
