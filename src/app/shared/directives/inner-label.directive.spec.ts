440568898a14caa7397d8987205065be738c6287
Swift <swiftx37@gmail.com>
2024-03-22 00:51:01 +0100

    The InnerLabel directive has been created.

---
+++ src/app/shared/directives/inner-label.directive.spec.ts
@@ -0,0 +1,8 @@
+import { InnerLabelDirective } from './inner-label.directive';
+
+// describe('InnerLabelDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new InnerLabelDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
