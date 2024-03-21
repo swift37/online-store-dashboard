55c20acdbc6547136a623404c915a0574c08721a
Swift <swiftx37@gmail.com>
2024-03-22 00:53:51 +0100

    The new directives have been declared and exported from the SharedModule. Some minor changes.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,24 @@ import { CommonModule } from '@angular/common';
 import { SvgIconComponent } from './components/svg-icon/svg-icon.component';
 import { SvgIconService } from './services/svg-icon.service';
 import { DefaultInputComponent } from './components/default-input/default-input.component';
+import { FilledStyleDirective } from './directives/filled-style.directive';
+import { InnerLabelDirective } from './directives/inner-label.directive';
 
 @NgModule({
   imports: [CommonModule],
-  exports: [CommonModule, SvgIconComponent, DefaultInputComponent],
-  declarations: [SvgIconComponent, DefaultInputComponent],
+  exports: [
+    CommonModule,
+    SvgIconComponent,
+    DefaultInputComponent,
+    InnerLabelDirective,
+    FilledStyleDirective,
+  ],
+  declarations: [
+    SvgIconComponent,
+    DefaultInputComponent,
+    InnerLabelDirective,
+    FilledStyleDirective,
+  ],
   providers: [SvgIconService],
 })
 export class SharedModule {}
