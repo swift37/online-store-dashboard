b148afb34aa82b156f1cd56e36a902fe42d6d1ba
Swift <swiftx37@gmail.com>
2024-08-20 13:14:42 +0200

    Config setup.

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,18 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { authHeaderInterceptor } from './core/auth/auth-header.interceptor';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), provideHttpClient(withFetch())],
+  providers: [
+    provideServerRendering(),
+    provideHttpClient(withFetch(), withInterceptors([authHeaderInterceptor])),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,32 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { authFeatureKey, authReducer } from './core/auth/store/auth.reducer';
+import * as authEffects from './core/auth/store/auth.effects';
+import { provideEffects } from '@ngrx/effects';
+import { authHeaderInterceptor } from './core/auth/auth-header.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(),
+    provideStore(),
+    provideState(authFeatureKey, authReducer),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+    provideEffects(authEffects),
+    //provideRouterStore(),
+    provideHttpClient(withInterceptors([authHeaderInterceptor])),
+    provideAnimations(),
   ],
 };
